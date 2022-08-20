87fcfcc8396399ea06ea9cc175fc1590851b5120
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-08-19 18:40:39 -0700

    testing

--- src/pages/App/App.test.js
+++ src/pages/App/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { add } from '../../../controllers/calculate';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('sums the numbers', () => {
+  const numbers = {first: '111', second: '222'}
+  expect(add(numbers)).toBe(333);
 });
+
