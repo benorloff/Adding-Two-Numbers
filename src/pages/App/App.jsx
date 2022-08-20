2d5fbc26bdfa480825d193e1835fcbf216916ed4
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-08-19 17:43:02 -0700

    clean up App.jsx

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,49 +1,12 @@
-import React, { useState } from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "../HomePage/HomePage";
-import SignupPage from "../SignupPage/SignupPage";
-import LoginPage from "../LoginPage/LoginPage";
-import userService from "../../utils/userService";
 
 function App() {
-  const [user, setUser] = useState(userService.getUser());
-
-  function handleSignUpOrLogin() {
-    setUser(userService.getUser());
-  }
-
-  function handleLogout() {
-    userService.logout();
-    setUser(null);
-  }
-
-  if (user) {
-    return (
-      <Routes>
-        <Route path="/" element={<HomePage handleLogout={handleLogout}/>} />
-        <Route
-          path="/login"
-          element={<LoginPage handleSignUpOrLogin={handleSignUpOrLogin} />}
-        />
-        <Route
-          path="/signup"
-          element={<SignupPage handleSignUpOrLogin={handleSignUpOrLogin} />}
-        />
-      </Routes>
-    );
-  }
 
   return (
     <Routes>
-      <Route
-        path="/login"
-        element={<LoginPage handleSignUpOrLogin={handleSignUpOrLogin} />}
-      />
-      <Route
-        path="/signup"
-        element={<SignupPage handleSignUpOrLogin={handleSignUpOrLogin} />}
-      />
       <Route path="/*" element={<HomePage />} />
     </Routes>
   );
